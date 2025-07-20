28def6c935023fca9e9f82c38bf6570ed0f201e5
keeve101-2 <snowyequations@gmail.com>
2025-07-20 13:42:27 +0800

    feat: ui changes, add feature to allow users to input their points to calculate possible check-in points left

--- ms-cw-checker.py
+++ ms-cw-checker.py
@@ -1,12 +1,14 @@
 import streamlit as st
 import pandas as pd
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, timezone
 
 st.set_page_config(layout="wide")
 # Config
-start_date = datetime(2025, 6, 11)  # Wednesday
-end_date = datetime(2025, 9, 23)  # Last week inclusive
-check_in_cutoff = datetime(2025, 7, 16)  # 200 points starts here
+start_date = datetime(2025, 6, 11).astimezone(timezone.utc)  # Wednesday
+end_date = datetime(2025, 9, 23).astimezone(timezone.utc)  # Last week inclusive
+check_in_cutoff = datetime(2025, 7, 16).astimezone(
+    timezone.utc
+)  # 200 points starts here
 
 tiers = ["-", "Bronze", "Silver", "Gold", "Emerald", "Diamond", "Challenger"]
 tiers_points = [0, 5000, 10000, 15000, 20000, 30000, 40000]
@@ -21,7 +23,7 @@ while current_start <= end_date:
 
 st.title("GMS Challenger World Points Calculator")
 
-tab1, tab2, tab3 = st.tabs(["Boss Missions", "Hunting Missions", "Level Missions"])
+tab1, tab2, tab3 = st.tabs(["Hunting Missions", "Boss Missions", "Level Missions"])
 
 bosses_points = {
     "cygnus": {"easy": 50, "normal": 100},
@@ -45,6 +47,75 @@ bosses_points = {
 }
 
 with tab1:
+    with st.container(border=True):
+        # Collect check-insV
+        check_in_data = []
+        cols_per_row_check_ins = 5
+        total_check_in_points = 0
+        
+        today = datetime.today().astimezone(timezone.utc)
+        total_possible_points_left = 0
+
+        for i in range(0, len(weeks), cols_per_row_check_ins):
+            cols = st.columns(cols_per_row_check_ins)
+
+            for col_num, col in enumerate(cols):
+                with col:
+                    if i + col_num < len(weeks):
+                        week_start, week_end = weeks[i + col_num]
+                        points_per_check_in = (
+                            100 if week_start < check_in_cutoff else 200
+                        )
+                        check_ins = st.radio(
+                            f"{week_start.strftime('%d %b')} – {week_end.strftime('%d %b')}",
+                            options=[0, 1, 2, 3, 4, 5],
+                            index=0,
+                            horizontal=True,
+                            key=f"check_ins_{i+col_num}",
+                        )
+                        weekly_points = min(check_ins, 5) * points_per_check_in
+                        check_in_data.append(
+                            {
+                                "Week Start": week_start.strftime("%d %b"),
+                                "Week End": week_end.strftime("%d %b"),
+                                "Check-ins": check_ins,
+                                "Points per Check-in": points_per_check_in,
+                                "Weekly Points": weekly_points,
+                            }
+                        )
+                        if week_end < today:
+                            continue
+
+                        effective_start = today if week_start < today else week_start
+                        days_left = min((week_end - effective_start).days + 1, 5)
+                        total_possible_points_left += days_left * points_per_check_in
+
+        # Display summary
+        df = pd.DataFrame(check_in_data)
+
+        total_check_in_points = df["Weekly Points"].sum()
+        
+    with st.expander(label="Want to know how many check-in points left till end of event?"):
+
+        col1, col2, col3 = st.columns(3)
+
+        with col1:
+            user_current_points = st.number_input(
+                "Your Current Points",
+                min_value=0,
+                step=100,
+                format="%d",
+                key="user_input_current_points",
+            )
+        projected_total = user_current_points + total_possible_points_left
+        
+        with col2:
+            st.metric(label=f"Hunting Mission Points Left ({today.strftime('%d %b')} to {end_date.strftime('%d %b')})", value=f"{total_possible_points_left:,}")
+        
+        with col3:
+            st.metric(label="Projected Points From Hunting Missions", value=f"{projected_total:,}")   
+
+with tab2:
     with st.container(border=True):
         # Store results
         selected_difficulties = {}
@@ -85,46 +156,6 @@ with tab1:
                 diffs_str = ", ".join(d.title() for d in diffs)
                 # st.write(f"**{boss.title()}** — {diffs_str} ({points} pts)")
 
-with tab2:
-    with st.container(border=True):
-        # Collect check-insV
-        check_in_data = []
-        cols_per_row_check_ins = 5
-        total_check_in_points = 0
-
-        for i in range(0, len(weeks), cols_per_row_check_ins):
-            cols = st.columns(cols_per_row_check_ins)
-
-            for col_num, col in enumerate(cols):
-                with col:
-                    if i + col_num < len(weeks):
-                        week_start, week_end = weeks[i + col_num]
-                        points_per_check_in = (
-                            100 if week_start < check_in_cutoff else 200
-                        )
-                        check_ins = st.radio(
-                            f"{week_start.strftime('%d %b')} – {week_end.strftime('%d %b')}",
-                            options=[0, 1, 2, 3, 4, 5],
-                            index=0,
-                            horizontal=True,
-                            key=f"check_ins_{i+col_num}",
-                        )
-                        weekly_points = min(check_ins, 5) * points_per_check_in
-                        check_in_data.append(
-                            {
-                                "Week Start": week_start.strftime("%d %b"),
-                                "Week End": week_end.strftime("%d %b"),
-                                "Check-ins": check_ins,
-                                "Points per Check-in": points_per_check_in,
-                                "Weekly Points": weekly_points,
-                            }
-                        )
-
-        # Display summary
-        df = pd.DataFrame(check_in_data)
-
-        total_check_in_points = df["Weekly Points"].sum()
-
 
 with tab3:
     with st.container(border=True):
@@ -133,7 +164,10 @@ with tab3:
         total_level_checkpoint_points = 0
         selected_levels = st.pills(
             label="Level Points",
-            options=[f"{level}: {points:,} points" for level, points in zip(level_checkpoints, level_checkpoints_points)],
+            options=[
+                f"{level}: {points:,} points"
+                for level, points in zip(level_checkpoints, level_checkpoints_points)
+            ],
             selection_mode="multi",
             format_func=lambda x: x,
             key="pills_level_points",
@@ -156,11 +190,11 @@ st.markdown("## Breakdown")
 with st.container(border=True):
     col1, col2, col3 = st.columns(3)
     with col1:
-        st.metric(label="Boss Points", value=f"{total_boss_points:,}")
+        st.metric(label="Hunting Missions", value=f"{total_check_in_points:,}")
     with col2:
-        st.metric(label="Check-in Points", value=f"{total_check_in_points:,}")
+        st.metric(label="Boss Missions", value=f"{total_boss_points:,}")
     with col3:
-        st.metric(label="Level Points", value=f"{total_level_checkpoint_points:,}")
+        st.metric(label="Level Missions", value=f"{total_level_checkpoint_points:,}")
 
 
 st.markdown("## Summary")
@@ -172,7 +206,7 @@ with st.container(border=True):
         st.metric(label="Current Tier", value=current_tier)
 
     with col2:
-        st.metric(label="Total Points", value=f"{total_points:,}")
+        st.metric(label="Total Reached", value=f"{total_points:,}")
 
     with col3:
         if current_tier != tiers[-1]:
